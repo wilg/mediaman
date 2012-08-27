3503bedf12057fb8ca9127c28178c2d3e7506e91
Wil Gieseler <supapuerco@gmail.com>
2012-08-26 21:01:50 -0700

    more failing specs

--- spec/mediaman/media_filename_spec.rb
+++ spec/mediaman/media_filename_spec.rb
@@ -29,6 +29,20 @@ describe Mediaman::MediaFilename do
       h = parse("John Adams.S01E01.720p.HDTV.x264")
       h[:name].should == "John Adams"
     end
+
+    it "Breaking Bad S01E04" do
+      h = parse("Breaking Bad S01E04")
+      h[:name].should == "Breaking Bad"
+      h[:episode_number].should == 4
+      h[:season_number].should == 4
+    end
+
+    it "My Best Friend 6x03" do
+      h = parse("My Best Friend 6x03")
+      h[:name].should == "My Best Friend"
+      h[:episode_number].should == 6
+      h[:season_number].should == 3
+    end
   
   end
   
